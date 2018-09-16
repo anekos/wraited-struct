4b6da6238c3f958cb42aa6eae7e060bbd9ef5989
anekos <anekos@snca.net>
2018-09-16 23:06:14 +0900

    Add document

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,27 @@ use std::{io, mem, slice};
 
 
 
-// http://stackoverflow.com/questions/25410028/how-to-read-a-struct-from-a-file-in-rust
+/// Read `T` with byte array
+///
+/// # Example
+///
+/// ```
+/// extern crate wraited_struct;
+/// use std::fs::File;
+///
+/// struct Customer {
+///     a: u8,
+///     b: u16,
+///     c: u32,
+/// }
+///
+/// fn main() {
+///     let mut file = File::open("customer.bin").unwrap();
+///     wraited_struct::read::<Customer, File>(&mut file).unwrap();
+/// }
+/// ```
 pub fn read<T, R: io::Read>(reader: &mut R) -> io::Result<T> {
+    // http://stackoverflow.com/questions/25410028/how-to-read-a-struct-from-a-file-in-rust
     let num_bytes = mem::size_of::<T>();
     unsafe {
         let mut result: T = mem::uninitialized();
@@ -20,6 +39,25 @@ pub fn read<T, R: io::Read>(reader: &mut R) -> io::Result<T> {
 }
 
 
+/// Write `T` with byte array
+///
+/// # Example
+///
+/// ```
+/// extern crate wraited_struct;
+/// use std::fs::File;
+///
+/// struct Customer {
+///     a: u8,
+///     b: u16,
+///     c: u32,
+/// }
+///
+/// fn main() {
+///     let mut file = File::create("customer.bin").unwrap();
+///     wraited_struct::write::<Customer, File>(&mut file, Customer { a: 97, b: 98, c: 99 }).unwrap();
+/// }
+/// ```
 pub fn write<T, W: io::Write>(writer: &mut W, mut value: T) -> io::Result<usize> {
     let num_bytes = mem::size_of::<T>();
     unsafe {
