7fe36e19867d0e1a3fc0f4086b91ef16ddb149a1
anekos <anekos@snca.net>
2018-09-16 23:14:15 +0900

    Add `unsafe` flag

--- src/lib.rs
+++ src/lib.rs
@@ -22,18 +22,16 @@ use std::{io, mem, slice};
 ///     wraited_struct::read::<Customer, File>(&mut file).unwrap();
 /// }
 /// ```
-pub fn read<T, R: io::Read>(reader: &mut R) -> io::Result<T> {
+pub unsafe fn read<T, R: io::Read>(reader: &mut R) -> io::Result<T> {
     // http://stackoverflow.com/questions/25410028/how-to-read-a-struct-from-a-file-in-rust
     let num_bytes = mem::size_of::<T>();
-    unsafe {
-        let mut result: T = mem::uninitialized();
-        let buffer: &mut [u8] = slice::from_raw_parts_mut(&mut result as *mut T as *mut u8, num_bytes);
-        match reader.read_exact(buffer) {
-            Ok(()) => Ok(result),
-            Err(e) => {
-                mem::forget(result);
-                Err(e)
-            }
+    let mut result: T = mem::uninitialized();
+    let buffer: &mut [u8] = slice::from_raw_parts_mut(&mut result as *mut T as *mut u8, num_bytes);
+    match reader.read_exact(buffer) {
+        Ok(()) => Ok(result),
+        Err(e) => {
+            mem::forget(result);
+            Err(e)
         }
     }
 }
@@ -58,10 +56,8 @@ pub fn read<T, R: io::Read>(reader: &mut R) -> io::Result<T> {
 ///     wraited_struct::write::<Customer, File>(&mut file, Customer { a: 97, b: 98, c: 99 }).unwrap();
 /// }
 /// ```
-pub fn write<T, W: io::Write>(writer: &mut W, mut value: T) -> io::Result<usize> {
+pub unsafe fn write<T, W: io::Write>(writer: &mut W, mut value: T) -> io::Result<usize> {
     let num_bytes = mem::size_of::<T>();
-    unsafe {
-        let buffer: &mut [u8] = slice::from_raw_parts_mut(&mut value as *mut T as *mut u8, num_bytes);
-        writer.write(buffer)
-    }
+    let buffer: &mut [u8] = slice::from_raw_parts_mut(&mut value as *mut T as *mut u8, num_bytes);
+    writer.write(buffer)
 }
